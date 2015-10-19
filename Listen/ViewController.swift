25768518ffd94790740355f9ef8f5166b5a99573
Stefan Trauth <mail@stefantrauth.de>
2015-10-19 15:57:30 +0200

    Initial Commit

---
+++ Listen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Listen
+//
+//  Created by Stefan Trauth on 19/10/15.
+//  Copyright © 2015 Stefan Trauth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
