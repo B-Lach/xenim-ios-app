5d5cab62ea7719297010a3710fcf644708183cc0
Stefan Trauth <mail@stefantrauth.de>
2015-10-24 17:39:04 +0200

    refactor player and implement play pause actions

--- Listen/PlayerViewController.swift
+++ Listen/PlayerViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
-//import AlamofireImage
+import AlamofireImage
 import AVFoundation
+import MediaPlayer
 
 class PlayerViewController: UIViewController {
 
@@ -19,63 +20,103 @@ class PlayerViewController: UIViewController {
     @IBOutlet weak var backgroundImageView: UIImageView!
     let miniCoverartImageView = UIImageView(image: UIImage(named: "event_placeholder"))
     
-    var player: AVPlayer?
+    @IBOutlet weak var playPauseButton: UIButton!
+    
+    var isPlaying:Bool {
+        get {
+                // rate is always between 0 and 1
+                // a rate greater than 0 means its playing
+                return player.rate > 0
+        }
+    }
+    var player = AVPlayer()
     var timer : NSTimer?
+    let imageCache = AutoPurgingImageCache()
 	
 	required init?(coder aDecoder: NSCoder) {
 		super.init(coder: aDecoder)
 		
 //		if UIScreen.mainScreen().traitCollection.userInterfaceIdiom == .Pad {
 
-        self.popupItem.rightBarButtonItems = [UIBarButtonItem(image: UIImage(named: "pause"), style: .Plain, target: nil, action: nil)]
+        self.popupItem.rightBarButtonItems = [UIBarButtonItem(image: UIImage(named: "pause"), style: .Plain, target: self, action: "pause")]
         
         miniCoverartImageView.frame = CGRectMake(0, 0, 30, 30)
         let popupItem = UIBarButtonItem(customView: miniCoverartImageView)
         self.popupItem.leftBarButtonItems = [popupItem]
         
         timer = NSTimer.scheduledTimerWithTimeInterval(60, target: self, selector: "_timerTicked:", userInfo: nil, repeats: true)
+        
+        let commandCenter = MPRemoteCommandCenter.sharedCommandCenter()
+        commandCenter.togglePlayPauseCommand.addTarget(self, action: "togglePlayPause")
+        commandCenter.togglePlayPauseCommand.enabled = true
 
 	}
     
     var event: Event! {
         didSet {
             updateUI()
+            player.replaceCurrentItemWithPlayerItem(AVPlayerItem(URL: event.streamurl))
             play()
         }
     }
 	
     override func viewDidLoad() {
         super.viewDidLoad()
+        UIApplication.sharedApplication().beginReceivingRemoteControlEvents()
         do {
             try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
         } catch {
             
         }
+        player = AVPlayer(URL: event.streamurl)
         updateUI()
         play()
 	}
     
-    func play(){
-        player = AVPlayer(URL: event.streamurl)
-        //self.player = AVPlayer(URL: NSURL(string: "http://detektor.fm/stream/mp3/musik/")!)
-        
-        if let player = player {
-            player.play()
-            //playButton.setTitle("Pause", forState: UIControlState.Normal)
+    func togglePlayPause() {
+        print("Toggled")
+    }
+    
+    func pause() {
+        player.pause()
+        self.popupItem.rightBarButtonItems = [UIBarButtonItem(image: UIImage(named: "play"), style: .Plain, target: self, action: "play")]
+        playPauseButton?.setImage(UIImage(named: "nowPlaying_play"), forState: UIControlState.Normal)
+    }
+    
+    @IBAction func togglePlayPause(sender: AnyObject) {
+        if isPlaying {
+            pause()
+        } else {
+            play()
         }
     }
     
+    func play(){
+        do {
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {}
+        player.play()
+        self.popupItem.rightBarButtonItems = [UIBarButtonItem(image: UIImage(named: "pause"), style: .Plain, target: self, action: "pause")]
+        playPauseButton?.setImage(UIImage(named: "nowPlaying_pause"), forState: UIControlState.Normal)
+
+    }
+    
     func updateUI() {
-        if let event = event {
-            podcastNameLabel?.text = event.title
-            popupItem.title = event.title
-            subtitleLabel?.text = event.description
-            popupItem.subtitle = event.description
-            coverartView?.af_setImageWithURL(event.imageurl, placeholderImage: UIImage(named: "event_placeholder"))
-            backgroundImageView?.af_setImageWithURL(event.imageurl, placeholderImage: UIImage(named: "event_placeholder"))
-            miniCoverartImageView.af_setImageWithURL(event.imageurl, placeholderImage: UIImage(named: "event_placeholder"))
-            _timerTicked(timer!)
-        }
+        podcastNameLabel?.text = event.title
+        popupItem.title = event.title
+        subtitleLabel?.text = event.description
+        popupItem.subtitle = event.description
+        coverartView?.af_setImageWithURL(event.imageurl, placeholderImage: UIImage(named: "event_placeholder"))
+        backgroundImageView?.af_setImageWithURL(event.imageurl, placeholderImage: UIImage(named: "event_placeholder"))
+        miniCoverartImageView.af_setImageWithURL(event.imageurl, placeholderImage: UIImage(named: "event_placeholder"))
+        _timerTicked(timer!)
+        
+        let songInfo: Dictionary = [
+            MPMediaItemPropertyTitle: event.title,
+            MPMediaItemPropertyArtist: event.description,
+            MPMediaItemPropertyArtwork: MPMediaItemArtwork(image: UIImage(named: "event_placeholder")!)
+        ]
+        MPNowPlayingInfoCenter.defaultCenter().nowPlayingInfo = songInfo
     }
 
 	override func preferredStatusBarStyle() -> UIStatusBarStyle {
