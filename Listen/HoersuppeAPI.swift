ac9e77acea320056923c3676a2ad97e11832e506
Stefan Trauth <mail@stefantrauth.de>
2015-11-24 12:00:10 +0100

    trim description and name strings provided by the API because they somtimes contain spaces and newlines

--- Listen/HoersuppeAPI.swift
+++ Listen/HoersuppeAPI.swift
@@ -34,9 +34,9 @@ class HoersuppeAPI {
                             let livedate = eventJSON["liveDate"].string!
                             let imageurl = eventJSON["imageUrl"].string!
                             let slug = eventJSON["podcast"].string!
-                            let description = eventJSON["description"].string!
+                            let description = eventJSON["description"].string!.trim()
                             let streamurl = eventJSON["streamUrl"].string!
-                            let title = eventJSON["eventTitle"].string!
+                            let title = eventJSON["eventTitle"].string!.trim()
                             let url = eventJSON["url"].string!
                             
                             if let event = Event(duration: duration, livedate: livedate, podcastSlug: slug, streamurl: streamurl, imageurl: imageurl, podcastDescription: description, title: title, url: url) {
@@ -63,13 +63,13 @@ class HoersuppeAPI {
                     let json = JSON(data: responseData)
                     let podcastJSON = json["data"]
                     
-                    let name = podcastJSON["title"].string!
-                    let subtitle = podcastJSON["subtitle"].string!
+                    let name = podcastJSON["title"].string!.trim()
+                    let subtitle = podcastJSON["subtitle"].string!.trim()
                     let url = podcastJSON["url"].string!
                     let feedurl = podcastJSON["feedurl"].string!
                     let imageurl = podcastJSON["imageurl"].string!
                     let slug = podcastJSON["slug"].string!
-                    let description = podcastJSON["description"].string!
+                    let description = podcastJSON["description"].string!.trim()
                     let chatServer = podcastJSON["chat_server"].string!
                     let chatChannel = podcastJSON["chat_channel"].string!
                     let webchatUrl = podcastJSON["chat_url"].string!
@@ -107,7 +107,7 @@ class HoersuppeAPI {
                             let livedate = eventJSON["livedate"].string!
                             let slug = eventJSON["podcast"].string!
                             let streamurl = eventJSON["streamurl"].string!
-                            let title = eventJSON["title"].string!
+                            let title = eventJSON["title"].string!.trim()
                             let url = eventJSON["url"].string!
                             
                             // TODO imageurl fix, as this is not how this initializer should be used
