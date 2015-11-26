6c38a2f4912fd9f5a9dcedb24a17943c15d203b9
Stefan Trauth <mail@stefantrauth.de>
2015-11-26 16:50:24 +0100

    add classes and exit segues

---
+++ Listen/PodcastTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PodcastTableViewCell.swift
+//  Listen
+//
+//  Created by Stefan Trauth on 26/11/15.
+//  Copyright © 2015 Stefan Trauth. All rights reserved.
+//
+
+import UIKit
+
+class PodcastTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
