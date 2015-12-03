57c11bb64319123c0f5ab9965af7751a9870347a
Stefan Trauth <mail@stefantrauth.de>
2015-12-03 16:05:27 +0100

    manage remote control commands and pass it to the player

--- Listen/AppDelegate.swift
+++ Listen/AppDelegate.swift
@@ -40,6 +40,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    override func remoteControlReceivedWithEvent(event: UIEvent?) {
+        if let event = event {
+            PlayerManager.sharedInstance.player.remoteControlReceivedWithEvent(event)
+        }
+    }
 
 
 }
