8aadd0f771636e134caaa3cfc3d412b3102c642e
Stefan Trauth <mail@stefantrauth.de>
2016-01-17 17:49:15 +0100

    favorite detail basic class

--- Xenim/FavoriteDetailViewController.swift
+++ Xenim/FavoriteDetailViewController.swift
@@ -12,10 +12,34 @@ class FavoriteDetailViewController: UIViewController {
     
     var podcast: Podcast!
 
+    @IBOutlet weak var coverartImageView: UIImageView!
+    @IBOutlet weak var podcastNameLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var nextDateLabel: UILabel!
+    @IBOutlet weak var favoriteButton: UIButton!
+    @IBOutlet weak var podcastDescriptionLabel: UILabel!
+    @IBOutlet weak var toolbar: UIToolbar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupNotifications()
+        
+        let placeholderImage = UIImage(named: "event_placeholder")!
+        if let imageurl = podcast.artwork.thumb180Url{
+            coverartImageView.af_setImageWithURL(imageurl, placeholderImage: placeholderImage, imageTransition: .CrossDissolve(0.2))
+        } else {
+            coverartImageView.image = placeholderImage
+        }
+        
+        podcastNameLabel.text = podcast.name
+        subtitleLabel.text = podcast.subtitle
+        podcastDescriptionLabel.text = podcast.podcastDescription
+        
+        updateNextDateLabel()
+        updateFavoriteButton()
+        
+        setupToolbar()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,15 +47,91 @@ class FavoriteDetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setupToolbar() {
+        var items = [UIBarButtonItem]()
+        let space = UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil)
+        
+        if podcast.websiteUrl != nil {
+            let websiteBarButton = UIBarButtonItem(image: UIImage(named: "scarlet-25-safari"), style: .Plain, target: self, action: "openWebsite")
+            items.append(websiteBarButton)
+            items.append(space)
+        }
+        
+        if podcast.twitterURL != nil {
+            let twitterBarButton = UIBarButtonItem(image: UIImage(named: "scarlet-25-twitter-square"), style: .Plain, target: self, action: "openTwitter")
+            items.append(twitterBarButton)
+            items.append(space)
+        }
+        
+        if podcast.feedUrl != nil {
+            let subscribeBarButton = UIBarButtonItem(image: UIImage(named: "scarlet-25-rss-square"), style: .Plain, target: self, action: "subscribe")
+            items.append(subscribeBarButton)
+            items.append(space)
+        }
+        
+        if podcast.email != nil {
+            let mailBarButton = UIBarButtonItem(image: UIImage(named: "scarlet-25-envelope"), style: .Plain, target: self, action: "sendMail")
+            items.append(mailBarButton)
+            items.append(space)
+        }
+        
+        // more
+        
+        // last item should not be a space, so remove it then
+        if items.last == space {
+            items.removeLast()
+        }
+        
+        toolbar.setItems(items, animated: true)
     }
-    */
+    
+    func updateFavoriteButton() {
+        if Favorites.fetch().contains(podcast.id) {
+            favoriteButton?.setImage(UIImage(named: "scarlet-25-star"), forState: .Normal)
+        } else {
+            favoriteButton?.setImage(UIImage(named: "scarlet-25-star-o"), forState: .Normal)
+        }
+    }
+    
+    func updateNextDateLabel() {
+        // TODO
+    }
+    
+    // MARK: Actions
+    
+    func sendMail() {
+        
+    }
+    
+    func openWebsite() {
+        
+    }
+    
+    func openTwitter() {
+        
+    }
+    
+    func subscribe() {
+        
+    }
+    
+    @IBAction func toggleFavorite(sender: AnyObject) {
+        Favorites.toggle(podcastId: podcast.id)
+    }
+    
+    // MARK: notifications
+    
+    func setupNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("favoritesChanged:"), name: "favoritesChanged", object: nil)
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    func favoritesChanged(notification: NSNotification) {
+        updateFavoriteButton()
+    }
 
 }
