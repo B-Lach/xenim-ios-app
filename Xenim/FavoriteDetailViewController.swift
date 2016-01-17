e913981a8905feac0df2413506bf51a4bc394a2d
Stefan Trauth <mail@stefantrauth.de>
2016-01-17 18:05:45 +0100

    add action functions

--- Xenim/FavoriteDetailViewController.swift
+++ Xenim/FavoriteDetailViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import SafariServices
+import MessageUI
 
-class FavoriteDetailViewController: UIViewController {
+class FavoriteDetailViewController: UIViewController, SFSafariViewControllerDelegate, MFMailComposeViewControllerDelegate {
     
     var podcast: Podcast!
 
@@ -97,26 +99,100 @@ class FavoriteDetailViewController: UIViewController {
         // TODO
     }
     
-    // MARK: Actions
-    
-    func sendMail() {
-        
-    }
+    // MARK: - Actions
     
     func openWebsite() {
-        
+        let svc = SFSafariViewController(URL: podcast.websiteUrl!)
+        svc.delegate = self
+        self.presentViewController(svc, animated: true, completion: nil)
     }
     
     func openTwitter() {
-        
+        let svc = SFSafariViewController(URL: podcast.twitterURL!)
+        svc.delegate = self
+        self.presentViewController(svc, animated: true, completion: nil)
+
     }
     
     func subscribe() {
+        let subscribeClients = podcast.subscribeURLSchemesDictionary!
+        let optionMenu = UIAlertController(title: nil, message: NSLocalizedString("podcast_detailview_subscribe_alert_message", value: "Choose Podcast Client", comment: "when the user clicks on the podcast subscribe button an alert view opens to choose a podcast client. this is the message of the alert view."), preferredStyle: .ActionSheet)
+        optionMenu.view.tintColor = Constants.Colors.tintColor
         
+        // create one option for each podcast client
+        for client in subscribeClients {
+            let clientName = client.0
+            let subscribeURL = client.1
+            
+            // only show the option if the podcast client is installed which reacts to this URL
+            if UIApplication.sharedApplication().canOpenURL(subscribeURL) {
+                let action = UIAlertAction(title: clientName, style: .Default, handler: { (alert: UIAlertAction!) -> Void in
+                    UIApplication.sharedApplication().openURL(subscribeURL)
+                })
+                optionMenu.addAction(action)
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: NSLocalizedString("cancel", value: "Cancel", comment: "cancel string"), style: .Cancel, handler: {
+            (alert: UIAlertAction!) -> Void in
+        })
+        optionMenu.addAction(cancelAction)
+        
+        self.presentViewController(optionMenu, animated: true, completion: nil)
+        
+    }
+    
+    func sendMail() {
+        if MFMailComposeViewController.canSendMail() {
+            let emailTitle = NSLocalizedString("podcast_detailview_feedback_mail_title", value: "Feedback", comment: "the user can send a feedback mail to the podcast. this is the preset mail title.")
+            let messageBody = NSLocalizedString("podcast_detailview_feedback_mail_body", value: "Hello,\n", comment: "mail body for a new feedback mail message")
+            let toRecipents = [podcast.email!]
+            
+            let mc: MFMailComposeViewController = MFMailComposeViewController()
+            mc.mailComposeDelegate = self
+            mc.setSubject(emailTitle)
+            mc.setMessageBody(messageBody, isHTML: false)
+            mc.setToRecipients(toRecipents)
+            
+            self.presentViewController(mc, animated: true, completion: nil)
+        } else {
+            // show error message if device is not configured to send mail
+            let message = NSLocalizedString("podcast_detailview_mail_not_supported_message", value: "Your device is not setup to send email.", comment: "the message shown to the user in an alert view if his device is not setup to send email")
+            showInfoMessage("Info", message: message)
+        }
+
+    }
+    
+    func showInfoMessage(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.view.tintColor = Constants.Colors.tintColor
+        let dismiss = NSLocalizedString("dismiss", value: "Dismiss", comment: "Dismiss")
+        alert.addAction(UIAlertAction(title: dismiss, style: UIAlertActionStyle.Cancel, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
     }
     
     @IBAction func toggleFavorite(sender: AnyObject) {
         Favorites.toggle(podcastId: podcast.id)
+    }
+    
+    // MARK: - delegate
+    
+    func safariViewControllerDidFinish(controller: SFSafariViewController) {
+        controller.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func mailComposeController(controller:MFMailComposeViewController, didFinishWithResult result:MFMailComposeResult, error:NSError?) {
+        switch result.rawValue {
+        case MFMailComposeResultCancelled.rawValue: break
+        case MFMailComposeResultSaved.rawValue: break
+        case MFMailComposeResultSent.rawValue: break
+        case MFMailComposeResultFailed.rawValue:
+            let mailFailureTitle = NSLocalizedString("info_message_mail_sent_failure_message", value: "Mail sent failure", comment: "If the user tried to sent an email and it could not be sent an alert view does show the error message. this is the title of the alert view popup")
+            showInfoMessage(mailFailureTitle, message: (error?.localizedDescription)!)
+        default:
+            break
+        }
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
     
     // MARK: notifications
