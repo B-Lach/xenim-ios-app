cee4353fdb464d4ba84eb24cfa2ee6bab03c78a1
Stefan Trauth <mail@stefantrauth.de>
2016-01-19 13:10:32 +0100

    change support mail

--- Xenim/SettingsTableViewController.swift
+++ Xenim/SettingsTableViewController.swift
@@ -61,7 +61,7 @@ class SettingsTableViewController: UITableViewController, SFSafariViewController
             let appVersionString = NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString")!
             let emailTitle = NSLocalizedString("settings_view_mail_title", value: "Listen Support", comment: "mail title for a new support mail message")
             let messageBody = NSLocalizedString("settings_view_mail_body", value: "Please try to explain your problem as detailed as possible, so we can find the best solution for your problem faster.\n\nInstalled Version: \(appVersionString)", comment: "mail body for a new support mail message")
-            let toRecipents = ["app-support@funkenstrahlen.de"]
+            let toRecipents = ["xenimapp@stefantrauth.de"]
             
             // configure mail compose view controller
             let mc: MFMailComposeViewController = MFMailComposeViewController()
