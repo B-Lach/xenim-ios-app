999d670137a83e11351e202d712d0f59ae0529a1
Stefan Trauth <mail@stefantrauth.de>
2016-01-19 12:03:06 +0100

    unwrap optional

--- Xenim/SettingsTableViewController.swift
+++ Xenim/SettingsTableViewController.swift
@@ -58,7 +58,7 @@ class SettingsTableViewController: UITableViewController, SFSafariViewController
         // check if the user is able to send mail
         if MFMailComposeViewController.canSendMail() {
             
-            let appVersionString = NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString")
+            let appVersionString = NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString")!
             let emailTitle = NSLocalizedString("settings_view_mail_title", value: "Listen Support", comment: "mail title for a new support mail message")
             let messageBody = NSLocalizedString("settings_view_mail_body", value: "Please try to explain your problem as detailed as possible, so we can find the best solution for your problem faster.\n\nInstalled Version: \(appVersionString)", comment: "mail body for a new support mail message")
             let toRecipents = ["app-support@funkenstrahlen.de"]
