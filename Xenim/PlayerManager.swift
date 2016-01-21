51bdb6809ab96e66ab13df9bf854ef95814fbca8
Stefan Trauth <mail@stefantrauth.de>
2016-01-21 11:41:13 +0100

    set player item descriptoin and title correctly

--- Xenim/PlayerManager.swift
+++ Xenim/PlayerManager.swift
@@ -136,8 +136,8 @@ class PlayerManager : NSObject, AudioPlayerDelegate, PlayerManagerDelegate {
             baseViewController?.popupBar!.addGestureRecognizer(longpressRecognizer)
             
             currentItem = audioItem
-            currentItem?.artist = event.podcast.podcastDescription
-            currentItem?.title = event.title
+            currentItem?.artist = event.title
+            currentItem?.title = event.eventDescription
             player.playItem(currentItem!) // save as this can not be nil
             
             // fetch coverart from image cache and set it as lockscreen artwork
