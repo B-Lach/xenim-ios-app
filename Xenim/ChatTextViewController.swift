da27cff80f7b8b9b5ea095ed09cfd01f91d852f9
Stefan Trauth <mail@stefantrauth.de>
2016-02-04 08:16:34 +0100

    textview basic table view methods

--- Xenim/ChatTextViewController.swift
+++ Xenim/ChatTextViewController.swift
@@ -8,19 +8,59 @@
 
 import SlackTextViewController
 
+struct Message {
+    var sender: String
+    var text: String
+    var date: NSDate
+}
+
 class ChatTextViewController: SLKTextViewController {
     
+    var messages = [Message]()
+    
     override class func tableViewStyleForCoder(decoder: NSCoder) -> UITableViewStyle {
-        return UITableViewStyle.Plain;
+        return UITableViewStyle.Plain
     }
     
     override func viewDidLoad() {
-        
-        // In progress in branch 'swift-example'
         super.viewDidLoad()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+    
+        return cell
+    }
+    
+    // MARK: - Text View Delegate
+    
+    override func didPressRightButton(sender: AnyObject!) {
+        // send message
+    }
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
 }
