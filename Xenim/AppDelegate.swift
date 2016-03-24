07cf9c81203af7186b995beafff357890fb48568
Stefan Trauth <mail@stefantrauth.de>
2016-03-24 11:38:46 +0100

    refresh events each time the app gets in foreground

--- Xenim/AppDelegate.swift
+++ Xenim/AppDelegate.swift
@@ -42,6 +42,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func applicationDidBecomeActive(application: UIApplication) {
         setupPushNotifications()
+        NSNotificationCenter.defaultCenter().postNotificationName("refreshEvents", object: nil, userInfo: nil)
         resetApplicationBadge(application)
     }
     
