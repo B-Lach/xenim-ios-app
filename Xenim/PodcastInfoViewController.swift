48cc4fd44d0fd7953579f4c0b650442c8d0ace9f
Stefan Trauth <mail@stefantrauth.de>
2016-03-25 17:51:07 +0100

    update favorite button

--- Xenim/PodcastInfoViewController.swift
+++ Xenim/PodcastInfoViewController.swift
@@ -47,6 +47,7 @@ class PodcastInfoViewController: UIViewController {
         podcastNameLabel?.text = event.podcast.name
         podcastDescriptionLabel?.text = event.podcast.podcastDescription
         eventTitleLabel?.text = event.title
+        updateFavoriteButton()
     }
 
     override func didReceiveMemoryWarning() {
@@ -57,13 +58,72 @@ class PodcastInfoViewController: UIViewController {
         statusBarStyleDelegate.updateStatusBarStyle(.LightContent)
     }
     
+    func updateFavoriteButton() {
+        if Favorites.fetch().contains(event.podcast.id) {
+            favoriteButton?.setImage(UIImage(named: "scarlet-44-star"), forState: .Normal)
+        } else {
+            favoriteButton?.setImage(UIImage(named: "scarlet-44-star-o"), forState: .Normal)
+        }
+    }
+    
+    // MARK: Actions
+    
     @IBAction func backToPlayer(sender: AnyObject) {
         pageViewDelegate.showPage(0)
     }
     
     @IBAction func toggleFavorite(sender: AnyObject) {
+        Favorites.toggle(podcastId: event.podcast.id)
     }
 
+    @IBAction func subscribe(sender: AnyObject) {
+    }
+    
+    
+    // MARK: notifications
+    
+    func setupNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(EventDetailViewController.favoriteAdded(_:)), name: "favoriteAdded", object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(EventDetailViewController.favoriteRemoved(_:)), name: "favoriteRemoved", object: nil)
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    func favoriteAdded(notification: NSNotification) {
+        if let userInfo = notification.userInfo, let podcastId = userInfo["podcastId"] as? String {
+            // check if this affects this cell
+            if podcastId == event.podcast.id {
+                favoriteButton?.setImage(UIImage(named: "scarlet-44-star"), forState: .Normal)
+                animateFavoriteButton()
+            }
+        }
+    }
+    
+    func favoriteRemoved(notification: NSNotification) {
+        if let userInfo = notification.userInfo, let podcastId = userInfo["podcastId"] as? String {
+            // check if this affects this cell
+            if podcastId == event.podcast.id {
+                favoriteButton?.setImage(UIImage(named: "scarlet-44-star-o"), forState: .Normal)
+                animateFavoriteButton()
+            }
+        }
+    }
+    
+    func animateFavoriteButton() {
+        favoriteButton.transform = CGAffineTransformMakeScale(1.3, 1.3)
+        UIView.animateWithDuration(0.3,
+                                   delay: 0,
+                                   usingSpringWithDamping: 2,
+                                   initialSpringVelocity: 1.0,
+                                   options: [UIViewAnimationOptions.CurveEaseOut],
+                                   animations: {
+                                    self.favoriteButton.transform = CGAffineTransformIdentity
+            }, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
