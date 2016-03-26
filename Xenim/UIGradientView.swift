fd81d0f7e359a87631e85371b425118cc66faaae
Stefan Trauth <mail@stefantrauth.de>
2016-03-26 09:35:27 +0100

    working gradient

---
+++ Xenim/UIGradientView.swift
@@ -0,0 +1,93 @@
+//
+//  UIGradientView.swift
+//  Xenim
+//
+//  Created by Stefan Trauth on 26/03/16.
+//  Copyright © 2016 Stefan Trauth. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class UIGradientView: UIView {
+    
+    /// Gradient layer that is added on top of the view
+    var gradientLayer: CAGradientLayer!
+    
+    /// Top color of the gradient layer
+    @IBInspectable var topColor: UIColor = UIColor.blackColor() {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    /// Bottom color of the gradient layer
+    @IBInspectable var bottomColor: UIColor = UIColor.clearColor() {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    /// At which vertical point the layer should end
+    @IBInspectable var bottomYPoint: CGFloat = 0.6 {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    func updateUI() {
+        setNeedsDisplay()
+    }
+    
+    /**
+     Sets up the gradient layer
+     */
+    func setupGradientLayer() {
+        gradientLayer = CAGradientLayer()
+        gradientLayer.frame = frame
+        gradientLayer.colors = [topColor.CGColor, bottomColor.CGColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0, y: bottomYPoint)
+        layer.addSublayer(gradientLayer)
+    }
+    
+    /**
+     Lays out all the subviews it has, in our case the gradient layer
+     */
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = frame
+    }
+    
+    /**
+     Initialises the view
+     
+     - parameter aDecoder: aDecoder
+     
+     - returns: self
+     */
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupGradientLayer()
+    }
+    
+    /**
+     Initialises the view
+     
+     - parameter frame: frame to use
+     
+     - returns: self
+     */
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupGradientLayer()
+    }
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
