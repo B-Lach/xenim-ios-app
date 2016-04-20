b84c7bb760dc79b2d24a02fdf6721839d5bcdd2d
Stefan Trauth <mail@stefantrauth.de>
2016-04-20 10:34:30 +0200

    generating screenshots for all devices automatically

--- Xenim/AppDelegate.swift
+++ Xenim/AppDelegate.swift
@@ -17,7 +17,7 @@ struct Constants {
     struct API {
         static let parseServer = "https://dev.push.xenim.de/parse"
         // "http://feeds.streams.demo.xenim.de/api/v1/"
-        static let xenimApiUrl = "http://feeds.streams.xenim.de/api/v1/"
+        static let xenimApiUrl = "http://feeds.streams.demo.xenim.de/api/v1/"
     }
 }
 
--- XenimUITests/XenimUITests.swift
+++ XenimUITests/XenimUITests.swift
@@ -30,35 +30,91 @@ class XenimUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testSnapshotsDE() {
+        if deviceLanguage != "de-DE" {
+            return
+        }
+        
+        let app = XCUIApplication()
+        snapshot("01Events")
+        
+        let tablesQuery = app.tables
+        tablesQuery.staticTexts["Lage der Nation"].tap()
+        snapshot("02PodcastDetail")
+        
+        app.navigationBars["Lage der Nation"].childrenMatchingType(.Button).matchingIdentifier("Zurück").elementBoundByIndex(0).tap()
+        tablesQuery.buttons["Play"].tap()
+        sleep(2)
+        snapshot("03Player")
+        
+        app.buttons["expand more"].tap()
+        app.toolbars.staticTexts["Testsendung"].pressForDuration(1.9);
+        let endPlaybackButton = app.sheets.collectionViews.buttons["Player beenden"]
+        endPlaybackButton.tap()
+
+        app.tabBars.buttons["Favoriten"].tap()
+        snapshot("04Favorites")
+        
+        let favoritenNavigationBar = app.navigationBars["Favoriten"]
+        favoritenNavigationBar.buttons["brandeis blue 25 plus"].tap()
+        snapshot("05AddFavorites")
+        
+        app.navigationBars["Favoriten finden"].buttons["Fertig"].tap()
+        favoritenNavigationBar.buttons["brandeis blue 25 gear"].tap()
+        snapshot("06Settings")
+    }
+    
+    func testSnapshotsEN() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         
+        if deviceLanguage != "en-US" {
+            return
+        }
         
         let app = XCUIApplication()
-        
         snapshot("01Events")
         
-        app.tables.staticTexts["Lage der Nation"].tap()
+        let tablesQuery = app.tables
+        tablesQuery.staticTexts["Lage der Nation"].tap()
         snapshot("02PodcastDetail")
         app.navigationBars["Lage der Nation"].childrenMatchingType(.Button).matchingIdentifier("Back").elementBoundByIndex(0).tap()
         
-        // TODOplayer
         
-        let tabBarsQuery = app.tabBars
-        tabBarsQuery.buttons["Favorites"].tap()
-        snapshot("03Favorites")
+        tablesQuery.buttons["Play"].tap()
+        sleep(2)
+        snapshot("03Player")
+        // close player
+        app.buttons["expand more"].tap()
+        app.toolbars.staticTexts["Testsendung"].pressForDuration(1.9);
+        let endPlaybackButton = app.sheets.collectionViews.buttons["End Playback"]
+        endPlaybackButton.tap()
+        
+        app.tabBars.buttons["Favorites"].tap()
+        snapshot("04Favorites")
         
         let favoritesNavigationBar = app.navigationBars["Favorites"]
         favoritesNavigationBar.buttons["brandeis blue 25 plus"].tap()
-        snapshot("04AddFavorites")
+        snapshot("05AddFavorites")
         app.navigationBars["Add Favorites"].buttons["Done"].tap()
         
         favoritesNavigationBar.buttons["brandeis blue 25 gear"].tap()
         snapshot("06Settings")
-        app.navigationBars["Settings"].buttons["Done"].tap()
-        tabBarsQuery.buttons["Events"].tap()
-        
     }
+    
+    //    func setupFavorites() {
+    //        let tabBarsQuery = app.tabBars
+    //        tabBarsQuery.buttons["Favorites"].tap()
+    //        app.navigationBars["Favorites"].buttons["brandeis blue 25 plus"].tap()
+    //
+    //        var tablesQuery = app.tables
+    //        tablesQuery.cells.containingType(.StaticText, identifier:"Freakshow").buttons["scarlet 44 star o"].tap()
+    //        tablesQuery.cells.containingType(.StaticText, identifier:"Lage der Nation").buttons["scarlet 44 star o"].tap()
+    //        tablesQuery.cells.containingType(.StaticText, identifier:"Not Safe For Work").buttons["scarlet 44 star o"].tap()
+    //        tablesQuery.staticTexts["Nerdemissionen"].swipeUp()
+    //        tablesQuery.cells.containingType(.StaticText, identifier:"Wrint").buttons["scarlet 44 star o"].tap()
+    //        app.navigationBars["Add Favorites"].buttons["Done"].tap()
+    //        tabBarsQuery.buttons["Events"].tap()
+    //    }
     
 }
