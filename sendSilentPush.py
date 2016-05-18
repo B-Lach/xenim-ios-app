3cd4840b33d69b0388f250b0de80e3f0b4a5036c
Stefan Trauth <mail@stefantrauth.de>
2016-05-18 12:39:11 +0200

    remove unused stuff in silent push

--- sendSilentPush.py
+++ sendSilentPush.py
@@ -1,21 +1,16 @@
 import json,httplib,plistlib
 
-podcast_name = "Lage der Nation"
-podcast_id = "498a7c0e-9fe7-45f3-8ec0-373d815983ab"
-
 # setup parse connection
 keys = plistlib.readPlist("Xenim/Keys.plist")
 parseApplicationID = keys["parseApplicationID"]
 # parseRestAPIKey = keys["parseRestAPIKey"]
 parseMasterKey = keys["parseMasterKey"]
-connection = httplib.HTTPSConnection('dev.push.xenim.de', 443)
+connection = httplib.HTTPSConnection('push.xenim.de', 443)
 connection.connect()
-
-message = podcast_name + " sendet jetzt live."
 
 connection.request('POST', '/parse/push', json.dumps({
        "where": {
-         
+
        },
        "data": {
 
