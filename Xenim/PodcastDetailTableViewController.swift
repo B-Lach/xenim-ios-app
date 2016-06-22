45d434fa6e4a27b980dec7f9fe4529a783c7bfb8
Stefan Trauth <mail@stefantrauth.de>
2016-06-22 13:46:04 +0200

    fix ipad rss subscribe crash

--- Xenim/PodcastDetailTableViewController.swift
+++ Xenim/PodcastDetailTableViewController.swift
@@ -142,6 +142,8 @@ class PodcastDetailTableViewController: UITableViewController, SFSafariViewContr
             })
             optionMenu.addAction(cancelAction)
             
+            optionMenu.popoverPresentationController?.sourceView = subscribeCell
+            
             self.presentViewController(optionMenu, animated: true, completion: nil)
         }
     }
