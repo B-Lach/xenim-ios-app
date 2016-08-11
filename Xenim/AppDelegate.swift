3e8e829337f1609a70e00f2623c4af40c2f651ff
Stefan Trauth <mail@stefantrauth.de>
2016-08-11 14:18:04 +0200

    move url handling code to its own section

--- Xenim/AppDelegate.swift
+++ Xenim/AppDelegate.swift
@@ -102,6 +102,49 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         // do not call refresh events here as didReceiveRemoteNotifications is called anyway
     }
     
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func setupPushNotifications() {
+        UNUserNotificationCenter.current().getNotificationSettings { (settings) in
+            switch settings.authorizationStatus {
+            case .notDetermined:
+                // push settings are not setup yet, so request authorization
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+                    if granted {
+                        UIApplication.shared.registerForRemoteNotifications()
+                    }
+                }
+            case .authorized:
+                // the user might have authorized notifications in the settings app while this app was closed
+                UIApplication.shared.registerForRemoteNotifications()
+            case .denied:
+                // the user denied notifications. if they were allowed before we do keep the push token on the server
+                // so push immediately starts working again if the user allows push notifications in the settings for
+                // this app again
+                break
+            }
+        }
+    }
+    
+    // MARK: - URL scheme handling and universal links
+    
+    
     func application(_ app: UIApplication, open url: URL, options: [String : AnyObject] = [:]) -> Bool {
         // this is called if the app is launched with an url scheme link clicked
         if url.scheme == "xenim" {
@@ -159,46 +202,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     
     func handleShowPodcastRequest(podcastID: String) {
         print("podcast: \(podcastID)")
-    }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-    
-    func setupPushNotifications() {
-        UNUserNotificationCenter.current().getNotificationSettings { (settings) in
-            switch settings.authorizationStatus {
-            case .notDetermined:
-                // push settings are not setup yet, so request authorization
-                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
-                    if granted {
-                        UIApplication.shared.registerForRemoteNotifications()
-                    }
-                }
-            case .authorized:
-                // the user might have authorized notifications in the settings app while this app was closed
-                UIApplication.shared.registerForRemoteNotifications()
-            case .denied:
-                // the user denied notifications. if they were allowed before we do keep the push token on the server
-                // so push immediately starts working again if the user allows push notifications in the settings for
-                // this app again
-                break
-            }
-        }
     }
 
 
